76bb06020874abff25341f86cdc64d3738d26f6c
Gabriella Capri <gcapriana@gmail.com>
2014-03-16 18:45:39 -0700

    psuedo code for updating the ranking table

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,3 +1,7 @@
 class Ranking < ActiveRecord::Base
 # Calculate that rating values ....
+
+# When a new row is created in the Ratings Table
+# Get Restaurant_ID and My_Rating_Value
+# Add My_Rating_Value to Total_Rating_Value for each Restaurant
 end
