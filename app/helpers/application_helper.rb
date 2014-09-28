6bc764b54ae742e19c7c616f12be71508ec2622d
Gabriella Capri <gcapriana@gmail.com>
2014-09-27 18:36:32 -0700

    Added a method to reformat the date and time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# the methodss added to this helper will be available
+# to all templates in the application; will be available in all views
 module ApplicationHelper
+  def format_time(time)
+	time_str = time.getlocal.strftime("%m/%d/%Y %I:%M:%S %p")
+	time_str.sub!(/^0/, '')
+	return time_str.downcase!
+  end
 end
